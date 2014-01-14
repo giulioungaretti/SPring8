0fa8486b2c2b03e0849a02df9217897458ad2cfa
Giulio D206 <giulio.ungaretti@gmail.com>
2014-01-15 00:45:34 +0100

    added new py script

---
+++ doplot.py
@@ -0,0 +1,14 @@
+from quickplot import loadme, do_diff_derivatives, shade
+import matplotlib.pyplot as plt
+name = 'InAs0799time3'
+data, parameters_dict, on_off, offset_list = loadme(name)
+fig, ax = do_diff_derivatives(
+    data, parameters_dict, 'TW',
+    name, False, True, True, True, True)
+fig, ax  = plt.subplots()
+ax.plot(data.index, data.Int_WZ,'o' )
+ax2 = ax.twinx()
+ax2.plot(data.index[data.NIG !=1],data.NIG[data.NIG !=1])
+shade(on_off, ax)
+
+fig.show()
---
+++ thermo.py
@@ -0,0 +1,2 @@
+import sympy as sym
+F,P,M,R,T = sym.symbols(['F','P','M','R','T'])
